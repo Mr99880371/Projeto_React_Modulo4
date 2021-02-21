b39889aee0263faeeed4f16ff5faefa6e1b56f82
Mr99880371 <mariane_araujo@id.uff.br>
2021-02-21 18:22:20 -0300

    Iniciando projeto com React

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import './App.css';
+
+
+function App() {
+    return ( <
+        div className = "App" >
+        <
+        Body / >
+        <
+        /div>
+    );
+}
+
+export default App;
+
+// import './App.css';
+
+// function App() {
+//     return ( <
+//         div className = "Contato" >
+//         <
+//         h1 > Contato < /h1> <
+//         label > Nome: < /label> <
+//         input type = "text" / >
+//         <
+//         label > Email: < /label> <
+//         input type = "text" / >
+//         <
+//         label > Mensagem: < /label> <
+//         textarea rows = "5"
+//         cols = "33" / >
+//         <
+//         /div>
+//     );
+// }
+
+// export default App;
